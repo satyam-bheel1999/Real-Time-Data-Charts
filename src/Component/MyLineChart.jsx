4bc7565f2d819a67aec88619984a8a9d8121a394
Satyam Bheel <136482276+satyam-bheel1999@users.noreply.github.com>
2025-01-21 14:51:48 +0530

    Update MyLineChart.jsx

--- src/Component/MyLineChart.jsx
+++ src/Component/MyLineChart.jsx
@@ -1,28 +1,10 @@
 import React, { useContext, useState } from 'react'
 import {Line} from 'react-chartjs-2';
-import {
-    Chart as ChartJS,
-    CategoryScale,
-    LinearScale,
-    LineElement,
-    PointElement,
-    Title,
-    Tooltip,
-    Legend,
-  } from 'chart.js';
-  
-  ChartJS.register(
-    CategoryScale,
-    LinearScale,
-    PointElement,
-    LineElement,
-    Title,
-    Tooltip,
-    Legend
-  );
+import 'chart.js/auto';
   
 
-
+function MyLineChart() {
+  const [data, setData] = useState([]);
   useContext(() => {
 
     let interval = setInterval(() => {
@@ -31,21 +13,16 @@ import {
       setData((prevData) => [...prevData, newData].slice(-20));
     }, 1000);
 
+    return () => clearInterval(interval);
+
   },[])
 
 const chartData = {
-    labels: ['January', 'February', 'March', 'April', 'May'],
+    labels: data.map((_, index) => index),
     datasets: [
       {
         label: 'My First dataset',
-        data: [65, 59, 70, 51, 40],
-        fill: false,
-        backgroundColor: 'rgba(75,192,192,0.4)',
-        borderColor: 'rgba(75,192,192,1)',
-      },
-      {
-        label: 'My First dataset',
-        data: [68, 55, 70, 81, 66],
+        data: data,
         fill: false,
         backgroundColor: 'rgba(75,192,192,0.4)',
         borderColor: 'rgba(75,192,192,1)',
@@ -55,11 +32,25 @@ const chartData = {
   
 const options = {
     responsive : true,
+    animation : { duration : 0,},
+    scales : {
+      x:{
+        display: true,
+        title: {
+          display: true,
+          text: 'Time (seconds)',
+        },
+        y:{
+          display: true,
+          title: {
+            display: true,
+            text: 'Value',
+          },
+        },
+      },
+    },
 }
   
-
-function MyLineChart() {
-  const [data, setData] = useState([]);
   return (
     <div>
         <Line data={chartData} options={options}/>
