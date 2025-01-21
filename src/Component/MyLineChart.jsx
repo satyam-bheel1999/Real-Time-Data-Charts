a32656e83972013bf2a888286ddd33b378f2dacd
Satyam Bheel <136482276+satyam-bheel1999@users.noreply.github.com>
2025-01-21 12:55:21 +0530

    Update MyLineChart.jsx

--- src/Component/MyLineChart.jsx
+++ src/Component/MyLineChart.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
 import {Line} from 'react-chartjs-2';
 import {
     Chart as ChartJS,
@@ -23,13 +23,30 @@ import {
   
 
 
+  useContext(() => {
+
+    let interval = setInterval(() => {
+      let newData = Math.random()*100;
+
+      setData((prevData) => [...prevData, newData].slice(-20));
+    }, 1000);
+
+  },[])
+
 const chartData = {
     labels: ['January', 'February', 'March', 'April', 'May'],
     datasets: [
       {
         label: 'My First dataset',
-        data: [65, 59, 80, 81, 56],
-        fill: true,
+        data: [65, 59, 70, 51, 40],
+        fill: false,
+        backgroundColor: 'rgba(75,192,192,0.4)',
+        borderColor: 'rgba(75,192,192,1)',
+      },
+      {
+        label: 'My First dataset',
+        data: [68, 55, 70, 81, 66],
+        fill: false,
         backgroundColor: 'rgba(75,192,192,0.4)',
         borderColor: 'rgba(75,192,192,1)',
       },
@@ -42,6 +59,7 @@ const options = {
   
 
 function MyLineChart() {
+  const [data, setData] = useState([]);
   return (
     <div>
         <Line data={chartData} options={options}/>
